293dac8be0e262b9ae0abb64886134cd5bda621e
sw961174314 <961174314@qq.com>
2024-11-05 15:34:09 +0800

    005.构建微服务并注册到Nacos中

--- chat-api/auth-service/src/main/java/com/chat/AuthApplicaiton.java
+++ chat-api/auth-service/src/main/java/com/chat/AuthApplicaiton.java
@@ -2,8 +2,11 @@ package com.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+// 开启服务的注册和发现功能
+@EnableDiscoveryClient
 public class AuthApplicaiton {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplicaiton.class, args);
--- chat-api/main-service/src/main/java/com/chat/MainApplication.java
+++ chat-api/main-service/src/main/java/com/chat/MainApplication.java
@@ -2,8 +2,11 @@ package com.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+// 开启服务的注册和发现功能
+@EnableDiscoveryClient
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
