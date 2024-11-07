48927012ba5d4860d0b5c39b9f71c38a4214d52d
sw961174314 <961174314@qq.com>
2024-11-07 13:39:55 +0800

    023.包装用户信息签发分布式令牌会话

--- chat-api/auth-service/src/main/java/com/chat/controller/PassportController.java
+++ chat-api/auth-service/src/main/java/com/chat/controller/PassportController.java
@@ -5,6 +5,7 @@ import com.chat.grace.result.ResponseStatusEnum;
 import com.chat.pojo.Users;
 import com.chat.pojo.bo.RegistLoginBO;
 import com.chat.grace.result.GraceJSONResult;
+import com.chat.pojo.vo.UsersVO;
 import com.chat.service.UsersService;
 import com.chat.tasks.SMSTask;
 import com.chat.utils.IPUtil;
@@ -14,10 +15,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @Slf4j
 @RestController
@@ -73,17 +77,26 @@ public class PassportController extends BaseInfoProperties {
             return GraceJSONResult.errorCustom(ResponseStatusEnum.SMS_CODE_ERROR);
         }
         // 2.根据mobile查询数据库，如果用户存在，则提示不能重复注册
-        Users users = usersService.queryMobileIfExist(mobile);
+        Users user = usersService.queryMobileIfExist(mobile);
         // 2.1 如果查询数据库中用户为空，表示用户没有注册过，则需要进行用户信息数据的入库
-        if (users == null) {
-            users = usersService.createUsers(mobile, nickName);
+        if (user == null) {
+            user = usersService.createUsers(mobile, nickName);
         } else {
             return GraceJSONResult.errorCustom(ResponseStatusEnum.USER_ALREADY_EXIST_ERROR);
         }
         // 3.用户注册成功后，删除Redis中的短信验证码使其失效
         redis.del(MOBILE_SMSCODE + ":" + mobile);
-        // 4.返回用户数据给前端
-        return GraceJSONResult.ok(users);
+        // 4.设置用户分布式会话，保存用户的token令牌，存储到Redis中
+        String uToken = TOKEN_USER_PREFIX + SYMBOL_DOT + UUID.randomUUID();
+        // 本方式只能限制用户在一台设备进行登录
+        // redis.set(REDIS_USER_TOKEN + ":" + user.getId(), uToken);   // 设置分布式会话
+        // 本方式允许用户在多端多设备进行登录
+        redis.set(REDIS_USER_TOKEN + ":" + uToken, user.getId());   // 设置分布式会话
+        // 5.返回用户数据给前端
+        UsersVO usersVO = new UsersVO();
+        BeanUtils.copyProperties(user, usersVO);
+        usersVO.setUserToken(uToken);
+        return GraceJSONResult.ok(usersVO);
     }
 
     /**
@@ -103,13 +116,22 @@ public class PassportController extends BaseInfoProperties {
         }
         // 2.根据mobile查询数据库，如果用户存在，则提示不能重复注册
         Users user = usersService.queryMobileIfExist(mobile);
-        // 2.1 如果查询数据库中用户为空，表示用户没有注册过，则需要进行用户信息数据的入库
+        // 2.1 如果查询数据库中用户为空，表示用户没有注册过，则需要返回错误信息
         if (user == null) {
             return GraceJSONResult.errorCustom(ResponseStatusEnum.USER_NOT_EXIST_ERROR);
         }
-        // 3.用户注册成功后，删除Redis中的短信验证码使其失效
+        // 3.用户登录成功后，删除Redis中的短信验证码使其失效
         redis.del(MOBILE_SMSCODE + ":" + mobile);
-        // 4.返回用户数据给前端
-        return GraceJSONResult.ok(user);
+        // 4.设置用户分布式会话，保存用户的token令牌，存储到Redis中
+        String uToken = TOKEN_USER_PREFIX + SYMBOL_DOT + UUID.randomUUID();
+        // 本方式只能限制用户在一台设备进行登录
+        // redis.set(REDIS_USER_TOKEN + ":" + user.getId(), uToken);   // 设置分布式会话
+        // 本方式允许用户在多端多设备进行登录
+        redis.set(REDIS_USER_TOKEN + ":" + uToken, user.getId());   // 设置分布式会话
+        // 5.返回用户数据给前端
+        UsersVO usersVO = new UsersVO();
+        BeanUtils.copyProperties(user, usersVO);
+        usersVO.setUserToken(uToken);
+        return GraceJSONResult.ok(usersVO);
     }
 }
