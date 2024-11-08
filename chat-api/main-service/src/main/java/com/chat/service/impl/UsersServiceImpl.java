b67fc3984773aad2e0e25461e538f769b4dd1bca
sw961174314 <961174314@qq.com>
2024-11-08 09:54:13 +0800

    026.限制微信号修改次数与时间

--- chat-api/main-service/src/main/java/com/chat/service/impl/UsersServiceImpl.java
+++ chat-api/main-service/src/main/java/com/chat/service/impl/UsersServiceImpl.java
@@ -27,8 +27,18 @@ public class UsersServiceImpl extends BaseInfoProperties implements UsersService
     public void modifyUserInfo(ModifyUserBO userBO) {
         // 用户id
         String userId = userBO.getUserId();
+        // 微信号
+        String wechatNum = userBO.getWechatNum();
+        // 判断用户id是否为空
         if (StringUtils.isBlank(userId)) {
             GraceException.display(ResponseStatusEnum.USER_INFO_UPDATED_ERROR);
+        }
+        // 判断微信号是否还在禁止修改的时间区域内
+        if (StringUtils.isNotBlank(wechatNum)) {
+            String isExist = redis.get(REDIS_USER_ALREADY_UPDATE_WECHAT_NUM + ":" + userId);
+            if (StringUtils.isNotBlank(isExist)) {
+                GraceException.display(ResponseStatusEnum.WECHAT_NUM_ALREADY_MODIFIED_ERROR);
+            }
         }
         Users pendingUser = new Users();
         pendingUser.setId(userId);
@@ -36,6 +46,10 @@ public class UsersServiceImpl extends BaseInfoProperties implements UsersService
         BeanUtils.copyProperties(userBO, pendingUser);
         // 将新的用户数据放入到数据库中
         usersMapper.updateById(pendingUser);
+        // 如果用户修改微信号，一年内只能修改一次，放入Redis进行判断
+        if (StringUtils.isNotBlank(wechatNum)) {
+            redis.setByDays(REDIS_USER_ALREADY_UPDATE_WECHAT_NUM + ":" + userId, userId, 365);
+        }
     }
 
     @Override
