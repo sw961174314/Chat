1f3f46932c515c2fdd929b24db22f7b45ee437fa
sw961174314 <961174314@qq.com>
2024-11-14 14:26:06 +0800

    073.实现语音发送功能

---
+++ chat-api/file-service/src/main/java/com/chat/util/BaiduAISDK.java
@@ -0,0 +1,7 @@
+package com.chat.util;
+
+public class BaiduAISDK {
+    public static final String APP_ID = "";
+    public static final String API_KEY = "";
+    public static final String SECRET_KEY = "";
+}
