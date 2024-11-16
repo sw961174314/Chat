3750caf7271888e1178a7e1c9a83f5625a8a8fcd
sw961174314 <961174314@qq.com>
2024-11-16 13:51:14 +0800

    084.通过接口来实现客户端负载均衡功能

---
+++ chat-api/chat-service/src/main/java/com/chat/netty/ChatApplication2.java
@@ -0,0 +1,81 @@
+package com.chat.netty;
+
+import com.chat.netty.util.JedisPoolUtils;
+import com.chat.netty.util.ZookeeperRegister;
+import com.chat.netty.websocket.WSServerInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import redis.clients.jedis.Jedis;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * Netty服务的启动类
+ */
+public class ChatApplication2 {
+
+    // netty默认端口
+    public static final Integer nettyDefaultPort = 875;
+    // 初始化连接数
+    public static final String initOnlineCounts = "0";
+
+    /**
+     * 动态分配Netty集群接口
+     * @param port
+     * @return
+     */
+    public static Integer selectPort(Integer port) {
+        String portKey = "netty_port";
+        Jedis jedis = JedisPoolUtils.getJedis();
+        Map<String, String> portMap = jedis.hgetAll(portKey);
+        // 由于map中的key都应该是整数类型的port，所以先转换成整数后，再进行对比，否则String类型的端口比对会有问题
+        List<Integer> portList = portMap.entrySet().stream().map(entry -> Integer.valueOf(entry.getKey())).collect(Collectors.toList());
+        Integer nettyPort = null;
+        if (portList == null || portList.isEmpty()) {
+            jedis.hset(portKey, port + "", initOnlineCounts);
+            nettyPort = port;
+        } else {
+            // 使用stream循环获得最大值，并且累加n，获得新的端口
+            Optional<Integer> maxInteger = portList.stream().max(Integer::compareTo);
+            Integer maxPort = maxInteger.get().intValue();
+            Integer currentPort = maxPort + 10;
+            jedis.hset(portKey, currentPort + "", initOnlineCounts);
+            nettyPort = currentPort;
+        }
+        return nettyPort;
+    }
+
+    public static void main(String[] args) throws Exception {
+        // 定义主线程组，用于接收客户端的连接，但是不做任何处理
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        // 定义从线程池，处理主线程池交过来的任务
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        // Netty服务启动的时候，从Redis中查找有没有端口，如果没有则使用875，如果有则端口累加10再启动
+        Integer nettyPort = selectPort(nettyDefaultPort);
+        // 向Zookeeper注册Netty服务节点
+        ZookeeperRegister.registerNettyServer("server-list", ZookeeperRegister.getLocalIP(), nettyPort);
+        try {
+            // 构建Netty服务器
+            ServerBootstrap server = new ServerBootstrap();
+            // 把主从线程池组放入到启动类中
+            // channel()设置Nio双向通道
+            // childHandler()设置处理器，主要用于处理workerGroup
+            server.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).childHandler(new WSServerInitializer());
+            // 启动server，并且绑定端口号为875，同时启动方式为同步
+            ChannelFuture channelFuture = server.bind(nettyPort).sync();
+            // 监听关闭的channel
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            // 关闭线程池组
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
--- chat-api/main-service/src/main/java/com/chat/controller/ChatController.java
+++ chat-api/main-service/src/main/java/com/chat/controller/ChatController.java
@@ -3,15 +3,17 @@ package com.chat.controller;
 import com.chat.base.BaseInfoProperties;
 import com.chat.grace.result.GraceJSONResult;
 import com.chat.pojo.netty.ChatMsg;
+import com.chat.pojo.netty.NettyServerNode;
 import com.chat.rabbitmq.RabbitMQTestConfig;
 import com.chat.service.ChatMessageService;
 import com.chat.utils.JsonUtils;
 import com.chat.utils.PagedGridResult;
 import jakarta.annotation.Resource;
+import org.apache.curator.framework.CuratorFramework;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("chat")
@@ -19,6 +21,9 @@ public class ChatController extends BaseInfoProperties {
 
     @Resource
     private ChatMessageService chatMessageService;
+
+    @Resource(name = "curatorClient")
+    private CuratorFramework zkClient;
 
     /**
      * 查询未读消息数量
@@ -69,5 +74,28 @@ public class ChatController extends BaseInfoProperties {
     public GraceJSONResult signRead(@PathVariable("msgId") String msgId) {
         chatMessageService.updateMsgSignRead(msgId);
         return GraceJSONResult.ok();
+    }
+
+    /**
+     * 从zookeeper中读取当前netty节点数据，以获取最少连接数返回给前端
+     * @return
+     * @throws Exception
+     */
+    @PostMapping("getNettyOnlineInfo")
+    public GraceJSONResult getNettyOnlineInfo() throws Exception {
+        // 从zookeeper中获得当前已经注册的netty服务列表
+        String path = "/server-list";
+        List<String> list = zkClient.getChildren().forPath(path);
+        List<NettyServerNode> serverNodeList = new ArrayList<>();
+        for (String node : list) {
+            // 获取节点数据
+            String nodeValue = new String(zkClient.getData().forPath(path + "/" + node));
+            NettyServerNode serverNode = JsonUtils.jsonToPojo(nodeValue, NettyServerNode.class);
+            serverNodeList.add(serverNode);
+        }
+        // 计算当前的zk的node哪个是最少连接数，以此获得[ip:port]返回给前端
+        Optional<NettyServerNode> minNodeOptional = serverNodeList.stream().min(Comparator.comparing(node -> node.getOnlineCounts()));
+        NettyServerNode minNode = minNodeOptional.get();
+        return GraceJSONResult.ok(minNode);
     }
 }
